5e3848b4035d98f508935f39880edc58e48bfc8c
Nurzhan Bazhirov <nbazhirov@cloudlinux.com>
2024-10-29 18:18:08 +0500

    add support for fanotify events marked by file handles

--- fanotify.go
+++ fanotify.go
@@ -1,5 +1,6 @@
 package fsnotify
 
+import "C"
 import (
 	"errors"
 	"fmt"
@@ -11,19 +12,91 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+type fanotifyEventInfoHeader struct {
+	InfoType uint8
+	Pad      uint8
+	Len      uint16
+}
+
+// fanotifyEventInfoFID represents fanotify_event_info_fid structure (see fanotify man page)
+type fanotifyEventInfoFID struct {
+	Hdr  fanotifyEventInfoHeader
+	Fsid unix.Fsid
+	// FileHandle starts here
+}
+
+const (
+	sizeOfFileHandleHdr        = C.sizeof_uint + C.sizeof_int
+	sizeOfFanotifyEventInfoFID = (int)(unsafe.Sizeof(fanotifyEventInfoFID{}))
+)
+
+func (f *fanotifyEventInfoFID) GetHandle(n int) (unix.FileHandle, error) {
+	fid := f
+	// Get pointer to the start of FileHandle
+	fileHandlePtr := unsafe.Pointer(uintptr(unsafe.Pointer(fid)) + unsafe.Sizeof(*fid))
+
+	// Check if hdr len exceeds n or has insufficient length
+	if int(fid.Hdr.Len) > n || int(fid.Hdr.Len) < sizeOfFanotifyEventInfoFID+sizeOfFileHandleHdr {
+		return unix.FileHandle{}, fmt.Errorf(
+			"GetHandle: out of bounds. Expected size n: %v, fid.Hdr.Len: %v",
+			n,
+			fid.Hdr.Len,
+		)
+	}
+	// The length of the buffer can be calculated from the Header's Len field
+	// Subtract the size of the header to get the FileHandle buffer length
+	bufferLen := int(fid.Hdr.Len) - sizeOfFanotifyEventInfoFID
+	// Create a slice from the pointer
+	buf := unsafe.Slice((*byte)(fileHandlePtr), bufferLen)
+
+	// Get size and type of file_handle
+	size := uint(*(*C.uint)(unsafe.Pointer(&buf[0])))
+	typ := int32(*(*C.int)(unsafe.Pointer(&buf[C.sizeof_uint])))
+
+	// Check if file_handle size is in bounds of n
+	bufferLen = sizeOfFanotifyEventInfoFID + sizeOfFileHandleHdr + int(size)
+	if bufferLen > n {
+		return unix.FileHandle{}, fmt.Errorf(
+			"GetHandle: out of bounds. Expected size: %v, actual size: %v",
+			n,
+			bufferLen,
+		)
+	}
+
+	return unix.NewFileHandle(typ, buf[sizeOfFileHandleHdr:sizeOfFileHandleHdr+int(size)]), nil
+}
+
+func getPathFromHandle(mountFd int, handle unix.FileHandle) (string, error) {
+	fd, err := unix.OpenByHandleAt(mountFd, handle, unix.O_PATH)
+	if err != nil {
+		return "", fmt.Errorf("open_by_handle_at failed: %v", err)
+	}
+	defer unix.Close(fd)
+
+	procPath := fmt.Sprintf("/proc/self/fd/%d", fd)
+	path := make([]byte, unix.PathMax)
+
+	n, err := unix.Readlink(procPath, path)
+	if err != nil {
+		return "", fmt.Errorf("readlink failed: %v", err)
+	}
+
+	return string(path[:n]), nil
+}
+
 type FanotifyWatcher struct {
 	Fd       int
+	mountFd  int
+	root     string
 	done     chan struct{} // Channel for sending a "quit message" to the reader goroutine
 	doneResp chan struct{} // Channel to respond to Close
 	Events   chan Event
 	Errors   chan error
 	poller   *FdPoller
-
-	MarkType uint
 }
 
-func NewFanotifyWatcher(markType uint) (*FanotifyWatcher, error) {
-	fd, err := unix.FanotifyInit(unix.FAN_CLASS_NOTIF, unix.O_RDONLY|unix.O_LARGEFILE)
+func NewFanotifyWatcher(flags, eventFFlags uint, root string) (*FanotifyWatcher, error) {
+	fd, err := unix.FanotifyInit(flags, eventFFlags)
 	if fd < 0 {
 		return nil, err
 	}
@@ -41,27 +114,38 @@ func NewFanotifyWatcher(markType uint) (*FanotifyWatcher, error) {
 		Events:   make(chan Event),
 		Errors:   make(chan error),
 		poller:   poller,
-		MarkType: markType,
+		root:     root,
 	}
 	go fw.readEvents()
 	return fw, nil
 }
 
-func (fw *FanotifyWatcher) Add(path string, mask uint) error {
-	maskF := unix.FAN_MARK_ADD | fw.MarkType | mask
+func (fw *FanotifyWatcher) Add(path string, mask uint64, flags uint) error {
+	flags |= unix.FAN_MARK_ADD
 	err := unix.FanotifyMark(
 		fw.Fd,
-		maskF,
-		unix.FAN_CLOSE_WRITE,
+		flags,
+		mask,
 		unix.AT_FDCWD,
 		path,
 	)
 	if err != nil {
-		log.Printf("unix.FanotifyMark(%d, %s|%d|%d=%d, FAN_CLOSE_WRITE, AT_FDCWD, %s) failed: %s\n", fw.Fd,
-			"FAN_MARK_ADD", fw.MarkType, mask, maskF,
+		log.Printf("unix.FanotifyMark(%d, flags=%s|%d, mask=%d, AT_FDCWD, %s) failed: %s\n", fw.Fd,
+			"FAN_MARK_ADD", flags, mask,
 			path, err.Error())
+		return err
 	}
-	return err
+	// Open/Refresh mountFd
+	if len(fw.root) > 0 {
+		mountFd, err := unix.Open(fw.root, unix.O_RDONLY, 0)
+		if err != nil {
+			log.Printf("Failed to get mount_fd of %v: %v", fw.root, err)
+			return err
+		} else {
+			fw.mountFd = mountFd
+		}
+	}
+	return nil
 }
 
 func (fw *FanotifyWatcher) readEvents() {
@@ -77,6 +161,7 @@ func (fw *FanotifyWatcher) readEvents() {
 	defer close(fw.Events)
 	defer unix.Close(fw.Fd)
 	defer fw.poller.Close()
+	defer unix.Close(fw.mountFd)
 
 	for {
 		// See if we have been closed.
@@ -145,17 +230,50 @@ func (fw *FanotifyWatcher) readEvents() {
 					return
 				}
 			}
-
-			path, errno := os.Readlink(fmt.Sprintf("/proc/self/fd/%d", raw.Fd))
-			if errno != nil {
-				select {
-				case fw.Errors <- errno:
-				case <-fw.done:
-					return
+			if raw.Fd == unix.FAN_NOFD {
+				fileHandleOffset := int(offset) + int(raw.Metadata_len)
+				if int(raw.Metadata_len)+sizeOfFanotifyEventInfoFID > int(raw.Event_len) {
+					offset += raw.Event_len
+					continue
 				}
-			}
+				// Get FID info
+				info := (*fanotifyEventInfoFID)(unsafe.Pointer(&buf[fileHandleOffset]))
+				if info.Hdr.InfoType == unix.FAN_EVENT_INFO_TYPE_FID {
+					handle, err := info.GetHandle(int(raw.Event_len) - int(raw.Metadata_len))
+					if err != nil {
+						select {
+						case fw.Errors <- err:
+						case <-fw.done:
+							return
+						}
+						offset += raw.Event_len
+						continue
+					}
+					path, err := getPathFromHandle(fw.mountFd, handle)
+					if err != nil {
+						if !errors.Is(unix.ESTALE, err) {
+							select {
+							case fw.Errors <- err:
+							case <-fw.done:
+								return
+							}
+						}
+					} else {
+						fw.Events <- newFanotifyFIDEvent(path, mask)
+					}
+				}
+			} else {
+				path, errno := os.Readlink(fmt.Sprintf("/proc/self/fd/%d", raw.Fd))
+				if errno != nil {
+					select {
+					case fw.Errors <- errno:
+					case <-fw.done:
+						return
+					}
+				}
 
-			fw.Events <- newFanotifyEvent(path, uintptr(raw.Fd))
+				fw.Events <- newFanotifyEvent(path, uintptr(raw.Fd))
+			}
 			offset += raw.Event_len
 		}
 	}
@@ -172,6 +290,18 @@ func (fw *FanotifyWatcher) isClosed() bool {
 
 func newFanotifyEvent(name string, fd uintptr) Event {
 	return Event{Name: name, Op: Write, File: os.NewFile(fd, name)}
+}
+
+func newFanotifyFIDEvent(name string, mask uint64) Event {
+	e := Event{Name: name, File: nil}
+	if mask&unix.FAN_MODIFY == unix.FAN_MODIFY || mask&unix.FAN_CLOSE_WRITE == unix.FAN_CLOSE_WRITE {
+		e.Op |= Write
+	}
+	if mask&unix.FAN_MOVE_SELF == unix.FAN_MOVE_SELF || mask&unix.FAN_MOVE == unix.FAN_MOVE ||
+		mask&unix.FAN_MOVED_TO == unix.FAN_MOVED_TO || mask&unix.FAN_MOVED_FROM == unix.FAN_MOVED_FROM {
+		e.Op |= Move
+	}
+	return e
 }
 
 func (fw *FanotifyWatcher) Close() error {
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/cloudlinux/fsnotify
 
-go 1.13
+go 1.17
 
 require golang.org/x/sys v0.0.0-20191005200804-aed5e4c7ecf9
